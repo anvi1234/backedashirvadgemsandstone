b66262c831c890dff272d57d90ca806314bf822d
Anveshika <anveshika.srivastava@epsilonium.com>
2026-02-20 01:01:27 +0530

    fixed issue of review

--- models/review.js
+++ models/review.js
@@ -4,7 +4,13 @@ const reviewSchema = new mongoose.Schema({
   productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   rating: { type: Number, min: 1, max: 5 },
-  comment: String
+  comment: String,
+   images: [
+    {
+      url: String,
+      public_id: String
+    }
+  ],
 }, { timestamps: true });
 
 module.exports = mongoose.model('Review', reviewSchema);
