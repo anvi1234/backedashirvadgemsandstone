46e6883238923951836c55e38042f32cb0182cbc
Anveshika <anveshika.srivastava@epsilonium.com>
2026-02-20 23:45:28 +0530

    fixed issue

--- utils/createadmin.js
+++ utils/createadmin.js
@@ -1,28 +1,43 @@
 require('dotenv').config();
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const User = require('../models/User.model');
+const User = require('../models/user');
 
-mongoose.connect(process.env.MONGO_URI);
+
 
 async function createAdmin() {
-  const adminExists = await User.findOne({ role: 'admin' });
-  if (adminExists) {
-    console.log('Admin already exists');
+  try {
+   await mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 30000,
+  socketTimeoutMS: 45000,
+  family: 4
+});
+    console.log("MongoDB Connected");
+
+    const adminExists = await User.findOne({ email: 'admin@spiritualstore.com' });
+
+    if (adminExists) {
+      console.log('Admin already exists');
+      process.exit();
+    }
+
+    const hashedPassword = await bcrypt.hash('Admin@123', 10);
+
+    await User.create({
+      name: 'Admin',
+      email: 'admin@spiritualstore.com',
+      password: hashedPassword,
+      role: 'admin'
+    });
+
+    console.log('Admin created successfully');
     process.exit();
+
+  } catch (error) {
+    console.error("Error:", error);
+    process.exit(1);
   }
-
-  const hashedPassword = await bcrypt.hash('Admin@123', 10);
-
-  await User.create({
-    name: 'Admin',
-    email: 'admin@spiritualstore.com',
-    password: hashedPassword,
-    role: 'admin'
-  });
-
-  console.log('Admin created successfully');
-  process.exit();
 }
 
 createAdmin();
